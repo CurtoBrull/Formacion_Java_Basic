48917c8d4692f50e0f7a62a7dd2b4eda81069cd9
Javier Curto Brull <curto.brull.javier@jcurto.es>
2022-10-05 17:26:45 +0200

    Ejercicio 3 OBC Java

---
+++ OBC_Ejercicios_Java/src/Ejercicio3/Main.java
@@ -0,0 +1,15 @@
+package Ejercicio3;
+
+public class Main {
+	public static void main(String[] args) {
+
+		String[] nombres = {"Javier", "Loli", "Izhan", "Iris"};
+
+		System.out.println("Los nombres son: ");
+
+		for (String nombre : nombres) {
+			System.out.print(nombre + " ");
+		}
+
+	}
+}
