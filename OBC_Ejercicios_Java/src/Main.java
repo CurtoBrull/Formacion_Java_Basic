388a6e49f897e963a8b28b03a313f2c67ec91d1b
Javier Curto Brull <curto.brull.javier@jcurto.es>
2022-10-05 16:16:18 +0200

    Ejercicio1 Java Basic

--- OBC_Ejercicios_Java/src/Main.java
+++ OBC_Ejercicios_Java/src/Main.java
@@ -1,6 +1,36 @@
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("Hello world!");
+		// Enteros
+
+		byte number = 1;
+		short number2 = 11;
+		int number3 = 111;
+		long number4 = 111111;
+
+		System.out.println("Numeros enteros: " + number + " , " + number2 + " , " + number3 + " , " + number4 + "\n");
+
+		// Decimales
+
+		float decimal = 1.1f;
+		double decimal2 = 9.99d;
+
+		System.out.println("Decimales: " + decimal + " , " + decimal2 + "\n");
+
+		// Strings
+
+		String nombre = "Javier";
+		String apellido = "Curto";
+
+		// Character
+		char caracter = 'a';
+
+		System.out.println("Strings: " + nombre + " " + apellido + "\n");
+		System.out.println("Caracter: " + caracter + "\n");
+
+		// Booleanos
+
+		boolean verdadero = true;
+		boolean falso = false;
 	}
 }
